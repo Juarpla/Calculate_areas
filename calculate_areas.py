67911faf20140b6d92ef117d712fac09c6d39d93
Juan Plasencia <juan.plasencia.11@gmail.com>
2023-05-29 00:30:15 -0500

    Adding pytho file

---
+++ calculate_areas.py
@@ -0,0 +1,33 @@
+def compute_area_square(side):
+    area = side**2
+    return area
+def compute_area_rectangle(length, width):
+    area = length*width
+    return area
+def compute_area_circle(radio):
+    area = 3.1415*(radio**2)
+    return area
+
+quit = False
+while quit == False:
+    shape = input("\nWhat kind of shape do you have?\n(square/circle/rectangle): ").lower()
+    if shape == "square":
+        square_side = float(input("\nEnter the side of a square: "))
+        result_square = round(compute_area_square(square_side),2)
+        print(f"The area of the square is {result_square}")
+        quit = True
+    elif shape == "circle":
+        circle_radio = float(input("\nEnter the radio of a circle: "))
+        result_circle = round(compute_area_circle(circle_radio),2)
+        print(f"The area of the cicle is {result_circle}")
+        quit = True
+    elif shape == "rectangle":
+        rectangle_length = float(input("\nEnter the length of a rectangle: "))
+        rectangle_width = float(input("Enter the width of a rectangle: "))
+        result_rectangle = round(compute_area_rectangle(rectangle_length, rectangle_width),2)
+        print(f"The area of the rectangle is {result_rectangle}")
+        quit = True
+    elif shape == "quit":
+        quit = True
+    else:
+        print("If you want it, you can type 'quit' to exit")   
