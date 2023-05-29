4e027edcb276cf068f5c81429fb06e4ef7b8ad6c
Juan Plasencia <juan.plasencia.11@gmail.com>
2023-05-29 00:36:56 -0500

    Adding commnets to the python file

--- calculate_areas.py
+++ calculate_areas.py
@@ -1,13 +1,18 @@
 def compute_area_square(side):
+    """That accepts a single value as a parameter, and then computes the area for the square and returns it."""
     area = side**2
     return area
 def compute_area_rectangle(length, width):
+    """It should pass the square side length to it (twice) and then return the value that the function computes"""
     area = length*width
     return area
 def compute_area_circle(radio):
+    """That accepts a single value as a parameter, and then computes the area for the circle and returns it."""
     area = 3.1415*(radio**2)
     return area
 
+#A loop to ask the user what kind of shape they have, then prompt for the length of a side, or sides, or radius, 
+#and then calls the appropriate function, and displays the result
 quit = False
 while quit == False:
     shape = input("\nWhat kind of shape do you have?\n(square/circle/rectangle): ").lower()
